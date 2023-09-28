84ca9480769d81f48dbe82f9281040a5772ca546
Senanmemmedov1990 <memmedov8665341@gmail.com>
2023-09-29 03:58:45 +0400

    tab menu

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+let tabs = document.querySelectorAll(".tabs div");
+let contents = document.querySelectorAll(".content div");
+
+ for (let tab of tabs) {
+    tab.addEventListener("click", function() {
+        let activeElement = document.querySelector(".active");
+        activeElement.classList.remove("active");
+        this.classList.add("active");
+
+        let index = this.getAttribute("data-index");
+        console.log(index);
+        for (let content of contents) {
+            if (content.getAttribute("data-index") == index) {
+                content.classList.add("show");
+            }
+            else{
+                 content.classList.remove("show");
+                }
+        }
+    })
+ }
